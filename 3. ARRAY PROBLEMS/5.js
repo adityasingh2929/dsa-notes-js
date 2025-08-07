850088da7010bf5594b3e8644a59f4aced2b37de
adityasingh2929 <2022aditya29@gmail.com>
2025-08-07 22:29:17 +0530

    Learned a bit more optimized method for array rotation, slow progress.

--- 3. ARRAY PROBLEMS/5.js
+++ 3. ARRAY PROBLEMS/5.js
@@ -1,41 +1,95 @@
 // left Rotate an array by 1 position.
-let ans = [1, 2, 3, 4, 5];
-let first = ans[0];
-for (let i = 1; i < ans.length; i++) {
-  ans[i - 1] = ans[i]; // my mistake was that I was iterating from 0 to n-1 and then i was putting arr[i] = arr[i-1] values. That just meant that I was just taking i-1 index's value untill the last by swapping.
+// let ans = [1, 2, 3, 4, 5];
+// let first = ans[0];
+// for (let i = 1; i < ans.length; i++) {
+//   ans[i - 1] = ans[i]; // my mistake was that I was iterating from 0 to n-1 and then i was putting arr[i] = arr[i-1] values. That just meant that I was just taking i-1 index's value untill the last by swapping.
+// }
+// ans[ans.length - 1] = first;
+// console.log(ans);
+
+// console.log();
+
+// // Right Rotate an array by 1 position.
+// let ansTwo = [1, 2, 3, 4, 5];
+// let last = ansTwo[ansTwo.length - 1];
+// for (let i = ansTwo.length - 2; i >= 0; i--) {
+//   ansTwo[i + 1] = ansTwo[i];
+// }
+// ansTwo[0] = last;
+// console.log(ansTwo);
+
+// console.log();
+
+// // Right Rotate an array by k positions.
+
+// // Brute-Forced approach:     (mine)
+// let arr = [1, 2, 3, 4, 5];
+// const n = arr.length;
+// let temp = 0;
+// let k = 1;
+// let prev = 0;
+// for (let c = 0; c < k; c++) {
+//   for (let i = 0; i < n; i++) {
+//     let prevIndex = (i + n - 1) % n;
+//     if (i == 0) prev = arr[prevIndex];
+//     temp = arr[i];
+//     arr[i] = prev;
+//     prev = temp;
+//   }
+// }
+
+// console.log(arr);
+
+// brute-forced approach (mine but inspired from striver)      -> PENDING.
+let arrVar = [10, 20, 30, 40, 50, 60, 70];
+let d = 3; // ans: [50,60,70,10,20,30,40]
+let size = arrVar.length;
+let tempoArr = new Array(d);
+for (let i = size - d; i < size; i++) {
+  tempoArr[i - (size - d)] = arrVar[i];
 }
-ans[ans.length - 1] = first;
-console.log(ans);
-
-console.log();
-
-// Right Rotate an array by 1 position.
-let ansTwo = [1, 2, 3, 4, 5];
-let last = ansTwo[ansTwo.length - 1];
-for (let i = ansTwo.length - 2; i >= 0; i--) {
-  ansTwo[i + 1] = ansTwo[i];
+let first = 0;
+for (let i = d; i < size; i++) {
+  if (i == d) first = arrVar[i];
+  arrVar[i] = arrVar[i - d];
 }
-ansTwo[0] = last;
-console.log(ansTwo);
+arrVar[size - 1] = first;
 
-console.log();
-
-// Right Rotate an array by k positions.
-
-// Brute-Forced approach:
-let arr = [1, 2, 3, 4, 5];
-const n = arr.length;
-let temp = 0;
-let k = 1;
-let prev = 0;
-for (let c = 0; c < k; c++) {
-  for (let i = 0; i < n; i++) {
-    let prevIndex = (i + n - 1) % n;
-    if (i == 0) prev = arr[prevIndex];
-    temp = arr[i];
-    arr[i] = prev;
-    prev = temp;
-  }
+for (let i = 0; i < d; i++) {
+  arrVar[i] = tempoArr[i];
 }
 
-console.log(arr);
+console.log(arrVar);
+
+// let size = arrVar.length;
+// for(let i=d;i<size;i++){
+//   arrVar[i-d] = arrVar[i];
+// }
+
+// for(let i=size-d;i<size;i++){
+//   arrVar[i] = tempoArr[i-(size-d)];
+// }
+
+// console.log(arrVar);
+
+// Left Rotate an array by k positions.
+
+// Brute-forced approach (striver's)
+
+// let arrVar = [10,20,30,40,50,60,70];
+// let d = 3;
+// let tempoArr = new Array(d);
+// for(let i=0;i<d;i++){
+//   tempoArr[i] = arrVar[i];
+// }
+
+// let size = arrVar.length;
+// for(let i=d;i<size;i++){
+//   arrVar[i-d] = arrVar[i];
+// }
+
+// for(let i=size-d;i<size;i++){
+//   arrVar[i] = tempoArr[i-(size-d)];
+// }
+
+// console.log(arrVar);
---
+++ 3. ARRAY PROBLEMS/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+ize = arrVar.length;
+// for(let i=d;i<size;i++){
+//   arrVar[i-d] = arrVar[i];
+// }
+
+
+// for(let i=size-d;i<size;i++){
+//   arrVar[i] = tempoArr[i-(size-d)];
+// }
+
+// console.
